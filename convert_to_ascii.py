2e9e40f500c85df1023cfe5773e3063ebaecbc1e
gas <gastonbengolea@gmail.com>
2015-08-11 19:02:11 -0300

    Limit buffer size

--- convert_to_ascii.py
+++ convert_to_ascii.py
@@ -6,7 +6,7 @@ import unidecode
 
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
-with codecs.open("corpus/tagged_corpus_unic.txt", "r", "utf-8") as corpus:
+with codecs.open("corpus/tagged_corpus_unic.txt", "r", "utf-8", buffering=4096) as corpus:
     for i, line in enumerate(corpus.readlines()):
         print unidecode.unidecode(line[:-1])
         if i % 10000 == 0:
