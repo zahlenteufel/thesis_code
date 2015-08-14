ebabab4a4846f7b84ea83e3fcd68620f9f2d833b
gas <gastonbengolea@gmail.com>
2015-08-14 19:46:02 -0300

    Unify the conversion between unicode and ascii

--- convert_to_ascii.py
+++ convert_to_ascii.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import codecs
-import unidecode
+from predict_this.text.word import to_ascii
 
 for i in xrange(10000):
     filename = "corpus/tagged_chunks/%s" % str(i).zfill(4)
     with codecs.open(filename, "r", "utf-8") as corpus:
         with open(filename + ".ascii", "w") as ascii_corpus:
             for line in corpus.readlines():
-                print >>ascii_corpus, unidecode.unidecode(line[:-1])
+                print >>ascii_corpus, to_ascii(line[:-1])
