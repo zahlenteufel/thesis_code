089e72fa4993e76342b12b036f25b23c94da33a6
gas <gastonbengolea@gmail.com>
2015-08-19 20:24:30 -0300

    Debug fngram output

--- predictor_tables.py
+++ predictor_tables.py
@@ -2,7 +2,6 @@ from predict_this.flm.flm_specification import FLM_Specification
 from predict_this.text.prediction_texts import PredictionTexts
 from predict_this.predictor.unigram_cache_predictor import UnigramCachePredictor
 from predict_this.predictor.human_predictor import HumanPredictor
-from predict_this.text.word import Word
 import sys
 from itertools import izip
 import argparse
@@ -10,6 +9,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Output table with the information of the predictors")
 parser.add_argument("-text_numbers", type=int, nargs="+", default=[1, 2, 3, 4, 5, 7, 8], help="numbers in [1,2,3,4,5,7,8]")
 parser.add_argument("-flm_model_filenames", nargs="*", default=[])
+parser.add_argument("-debug_fngram", type=bool, default=False)
 args = parser.parse_args()
 
 prediction_texts = PredictionTexts(args.text_numbers)
@@ -20,7 +20,7 @@ predictors = [HumanPredictor(), UnigramCachePredictor()] + \
 print "word", " ".join([predictor.name() for predictor in predictors])
 
 # print table
-all_predictions = zip(*[predictor.batch_predict(prediction_texts) for predictor in predictors])
+all_predictions = zip(*[predictor.batch_predict(prediction_texts, debug=args.debug_fngram) for predictor in predictors])
 for target_word, predictions in izip(prediction_texts.target_words(), all_predictions):
     print target_word, " ".join(map(str, predictions))
     sys.stdout.flush()
