797951d20cab5c69f0c58465281252b4444ce55c
Tomas Geffner <gastonbengolea@gmail.com>
2015-08-26 16:01:34 -0300

    Convert to ascii script restored

---
+++ convert_to_ascii.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import io
+from predict_this.text.word import to_ascii
+
+for i in [3619]:
+    filename = "corpus/tagged_chunks/%s" % str(i).zfill(4)
+    with io.open(filename, "r", encoding="utf-8") as corpus:
+        with open(filename + ".ascii", "w") as ascii_corpus:
+            for line in corpus:
+                print >>ascii_corpus, to_ascii(line[:-1])
