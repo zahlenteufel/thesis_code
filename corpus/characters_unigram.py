c1c77ac0a3110078ce9f8fd605564357afdf6c01
gas <gastonbengolea@gmail.com>
2015-08-25 22:26:02 -0300

    Use io instead of codecs
    This way we don't depend on a third party library, and also our code is more
    compatible with Python 3.
    
    Also removed uses of readlines(), it's no use since a file is already a line
    iterator and readlines() loads everything in memory, which we definitely don't want
    in this use case.

--- corpus/characters_unigram.py
+++ corpus/characters_unigram.py
@@ -1,22 +1,21 @@
 # eliminate unicode invisible characters, converting them to spaces
-# import unicodedata
-import codecs
+import io
 import sys
 
-sys.stdin = codecs.getreader('utf8')(sys.stdin)
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+stdin = io.open(sys.stdin.fileno(), encoding="utf-8")
+stdout = io.open(sys.stdout.fileno(), encoding="utf-8")
 
 hist = dict()
 
 print >>sys.stderr, "populating histogram"
 
-for l in sys.stdin.readlines():
-    for c in l[:-1]:
-        hist[ord(c)] = hist.get(ord(c), 0) + 1
+for line in stdin:
+    for character in line[:-1]:
+        hist[ord(character)] = hist.get(ord(character), 0) + 1
 
 print >>sys.stderr, "hist populated"
 
 L = sorted([(v, k) for k, v in hist.iteritems()], reverse=True)
 
 for index, (count, letter) in enumerate(L):
-    print count, unichr(letter), "(%s)" % unichr(letter).encode("ascii", "backslashreplace")
+    stdout.write(u"%d %c %s \"(%s)\\n" % (count, unichr(letter), unichr(letter).encode("ascii", "backslashreplace")))
