5d522a785810097ad4f153c38546bc89ee011efe
Juan <juan@jah.(none)>
2015-08-27 17:09:45 -0300

    Put progressbar

--- corpus/factors_zip_lemmas.py
+++ corpus/factors_zip_lemmas.py
@@ -1,18 +1,23 @@
 from itertools import imap
+import progressbar
 
+progress_bar = progressbar.ProgressBar()
+progress_bar.maxval = 10000
+progress_bar.start()
 
 def get_lemmas():
     for i in xrange(10000):
+        progress_bar.update(i)
         with open("tagged_chunks/%s.ascii" % str(i).zfill(4)) as f:
             for line in f:
                 if len(line) != 1:
-                    lemmas = line.split()[1].replace(":", "_").split("_")
+                    lemmas = line.split()[1].split("_")
                     for lemma in lemmas:
-                        yield lemma
+                        yield lemma.split("+")[0]
+    progress_bar.finish()
 
 
 lemmas = get_lemmas()
-
 
 def insert_lemma_as_factor(factors, lemma):
     f = factors.split(":")
