aa7bf0f6b7a291775c3350b931e935707beac3e2
gas <gastonbengolea@gmail.com>
2015-09-09 11:55:01 -0300

    Gnerate a Table for only pobabilities

--- analize_table.py
+++ analize_table.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
-from itertools import izip
 from scipy.special import logit
 from scipy.stats.stats import pearsonr
-import math
 import sys
 
 # USAGE:
@@ -13,29 +11,18 @@ def select(table, column):
     return [row[column] for row in table]
 
 
-def filter_valid(x1, x2):
-    return zip(*[
-        (e1, e2) for e1, e2 in izip(x1, x2) if
-        not math.isinf(e1) and
-        not math.isinf(e2) and
-        not math.isnan(e1) and
-        not math.isnan(e2)])
-
 header = sys.stdin.readline()[:-1].split()[1:]
-table_probs = []
-table_ppls = []
+table = []
 while True:
-    row = sys.stdin.readline()[:-1].split()
+    row = sys.stdin.readline()[:-1].split(",")
     if not row:
         break
-    word, nums = row[0], map(lambda p: tuple(map(float, p.split(","))), row[1:])
+    word_info, probs = row[:3], map(float, row[3:])
 
-    probs, ppls = zip(*nums)
     if any(map(lambda x: x == 0, probs)):
-        print word, probs
+        print >>sys.stderr, "Error in word", word_info[1:3], probs
 
-    table_probs.append(probs)
-    table_ppls.append(ppls)
+    table.append(probs)
 
 
 def plot(x, y, xlabel, ylabel, title):
@@ -57,13 +44,6 @@ for i in xrange(1, n):
     name = header[0]
     name2 = header[i]
 
-    cloze_prob = select(table_probs, 0)
-    prob = select(table_probs, i)
-    cloze_prob, prob = filter_valid(cloze_prob, prob)
-    # print sorted(cloze_prob, reverse=True)
-    # print sorted(prob, reverse=True)
-    # logit_plot(cloze_prob, prob, name, name2, "probabilities")
+    cloze_prob, prob = select(table, 0), select(table, i)
 
-    # cloze_ppl = select(table_ppls, 0)
-    # yppl = select(table_ppls, i)
-    # plot(cloze_ppl, yppl, name, name2, "perplexity")
+    logit_plot(cloze_prob, prob, name, name2, "probabilities")
--- predictor_tables.py
+++ predictor_tables.py
@@ -8,41 +8,46 @@ import io
 from itertools import izip
 import argparse
 
-stdout = io.open(sys.stdout.fileno(), "w", encoding="utf-8")
 
-parser = argparse.ArgumentParser(description="Output table with the information of the predictors")
-parser.add_argument("-text_numbers", type=int, nargs="+", default=[1, 2, 3, 4, 5, 7, 8], help="numbers in [1,2,3,4,5,7,8]")
-parser.add_argument("-ngram_predictor_orders", type=int, nargs="*", default=[])
-parser.add_argument("-flm_model_filenames", nargs="*", default=[])
-parser.add_argument("-debug", type=bool, default=False)
-args = parser.parse_args()
+def print_predictor_tables():
+    stdout = io.open(sys.stdout.fileno(), "w", encoding="utf-8")
+    args = argument_parser()
+    prediction_texts = PredictionTexts(args.text_numbers)
+    predictors = [HumanPredictor(), UnigramCachePredictor()] + \
+        map(NgramPredictor, args.ngram_predictor_orders) + \
+        [FLM_Specification(flm_model_filename).predictor() for flm_model_filename in args.flm_model_filenames]
+    print_table(stdout, predictors, prediction_texts)
 
-prediction_texts = PredictionTexts(args.text_numbers)
-predictors = [HumanPredictor(), UnigramCachePredictor()] + \
-    map(NgramPredictor, args.ngram_predictor_orders) + \
-    [FLM_Specification(flm_model_filename).predictor() for flm_model_filename in args.flm_model_filenames]
 
-SEPARATOR = u","
+def argument_parser():
+    parser = argparse.ArgumentParser(description="Output table with the probabilities given by the predictors")
+    parser.add_argument("-text_numbers", type=int, nargs="+", default=[1, 2, 3, 4, 5, 7, 8], help="numbers in [1,2,3,4,5,7,8]")
+    parser.add_argument("-ngram_predictor_orders", type=int, nargs="*", default=[])
+    parser.add_argument("-flm_model_filenames", nargs="*", default=[])
+    parser.add_argument("-debug", type=bool, default=False)
+    return parser.parse_args()
 
-# print header
-stdout.write(SEPARATOR.join(["#texto", "#palabra", "palabra"] + [predictor.name() for predictor in predictors]) + "\n")
 
-all_predictions = []
-# print table
-for predictor in predictors:
-    print >>sys.stderr, predictor.name()
-    all_predictions.append(predictor.batch_predict(prediction_texts, args.debug))
-all_predictions = zip(*all_predictions)
+def print_table(file, predictors, prediction_texts):
+    header = ["#texto", "#palabra", "palabra"] + [predictor.name() for predictor in predictors]
+    print_row(file, header)
 
-for target_word, predictions in izip(prediction_texts.target_words(), all_predictions):
-    # print predictions
-    stdout.write(SEPARATOR.join(
-        map(unicode, [
-            target_word.text_index(),
-            target_word.word_index(),
-            target_word.original_word(),
-        ] +
-            map(lambda prediction: unicode(prediction[0]), predictions)
-            # don't print perplexity (prediction[1]) just yet
-        )
-    ) + "\n")
+    for target_word, predictions in izip(prediction_texts.target_words(), predictions_table(predictors, prediction_texts)):
+        fields = [target_word.text_index(), target_word.word_index(), target_word.original_word()] + predictions
+        print_row(file, fields)
+
+
+def print_row(file, row, separator=u","):
+    file.write(separator.join(map(unicode, row)) + u"\n")
+
+
+def predictions_table(predictors, prediction_texts):
+    return transpose([predictor.batch_predict(prediction_texts) for predictor in predictors])
+
+
+def transpose(table):
+    return zip(*table)
+
+
+if __name__ == "__main__":
+    print_predictor_tables()
