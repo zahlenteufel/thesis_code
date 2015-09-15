02711f902b5811062eeebe6daa0e0d0754012d94
gas <gastonbengolea@gmail.com>
2015-09-15 12:46:18 -0300

    Working model installer

--- install_model.py
+++ install_model.py
@@ -2,22 +2,23 @@ import sys
 import os
 import os.path
 from itertools import ifilter
+from itertools import imap
 
 
 def install_model(name, model_filename, destination_folder="flm_models/"):
     target_factor_file_filename = destination_folder + name + ".flm"
     with open(model_filename) as original_flm_spec_file:
-        flm_spec = ifilter(meaningful_line, original_flm_spec_file)
+        flm_spec = imap(lambda s: s.rstrip("\n"), ifilter(is_meaningful, original_flm_spec_file))
         with open(target_factor_file_filename, "w") as new_flm_spec:
             num = int(flm_spec.next())
             assert num == 1
-            print >>new_flm_spec, num
             header = flm_spec.next().split()
             if header[0].endswith(":"):
                 header = [header[0][:-1], ":"] + header[1:]
-            num_backoff_lines = header[-1]
+            num_backoff_lines = int(header[-1])
             header[-2] = destination_folder + name + ".lm.gz"
             header[-3] = destination_folder + name + ".counts.gz"
+            print >>new_flm_spec, 1
             print >>new_flm_spec, " ".join(header)
             for i in xrange(num_backoff_lines):
                 print >>new_flm_spec, flm_spec.next()
@@ -29,10 +30,8 @@ def install_model(name, model_filename, destination_folder="flm_models/"):
         os.system("ln -v " + model_filename_base + ".lm.gz " + destination_folder + name + ".lm.gz")
 
 
-def meaningful_line(it):
-    for line in it:
-        if line != "\n" and not line.startswith("##"):
-            yield line[:-1]
+def is_meaningful(line):
+    return line != "\n" and not line.startswith("##")
 
 
 if __name__ == "__main__":
