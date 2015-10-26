e1211be2160560d8cbe2bae161ab0702b32e6172
gas <gastonbengolea@gmail.com>
2015-10-26 13:10:22 -0300

    Remove old scripts

--- ngram_server.py
+++
@@ -1,8 +0,0 @@
-from SimpleXMLRPCServer import SimpleXMLRPCServer
-from utils import get_working_model
-from predictor import Predictor
-
-server = SimpleXMLRPCServer(("localhost", 9999), allow_none=True)
-server.register_introspection_functions()
-server.register_instance(Predictor(*get_working_model(7)))
-server.serve_forever()
