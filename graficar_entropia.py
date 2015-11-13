294c58d1df4346e531398d36895d24759ecb17e0
gas <gastonbengolea@gmail.com>
2015-11-13 14:15:08 -0300

    Scatter plot with logit option

--- graficar_entropia.py
+++ graficar_entropia.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
+from scipy.special import logit
 
 
-def plot_cols(colname1, colname2, file):
+def plot_cols(colname1, colname2, file, logit_scale=False):
     with open(file) as f:
         header = f.readline()[:-1].split(",")
-    plot(header.index(colname1), header.index(colname2), file)
+    plot(header.index(colname1), header.index(colname2), file, logit_scale)
 
 
-def plot(ind1, ind2, file):
+def plot(ind1, ind2, file, logit_scale=False):
     f = open(file)
 
     header = f.readline()[:-1].split(",")
@@ -16,20 +17,16 @@ def plot(ind1, ind2, file):
 
     for i, line in enumerate(f.readlines()):
         fields = line[:-1].split(",")
-        # print i, fields
         x.append(float(fields[ind1]))
         y.append(float(fields[ind2]))
 
-    plt.xlabel(header[ind1])
-    plt.ylabel(header[ind2])
-    plt.scatter(x, y)
+    if logit_scale:
+        x = logit(x)
+        y = logit(y)
+    plt.xlabel(header[ind1] + ("(logit)" if logit_scale else ""))
+    plt.ylabel(header[ind2] + ("(logit)" if logit_scale else ""))
+    plt.scatter(x, y, alpha=0.05)
     plt.show()
 
-# texto,#palabra,palabra,cloze_entropy,zeros,2gram_entropy,#texto,#palabra,
-# palabra,prob_cloze,prob_cache,prob2gram
-
-# texto,#palabra,palabra,cloze_predictor,unigram_cache_predictor,4gram
-
-# plot_cols("cloze_predictor", "prob_cloze", "entropias")
-plot(1, 0, "prob_ent4.csv")
+plot_cols("prob_cloze", "prob_4gram", "analisis_4gram.csv", logit_scale=True)
 plt.close()
