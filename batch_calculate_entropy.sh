6b7ce3a9e6670207870ff6701ca5202ffd5c9957
gas <gastonbengolea@gmail.com>
2015-11-27 16:03:33 -0300

    dump tables

--- batch_calculate_entropy.sh
+++ batch_calculate_entropy.sh
@@ -20,9 +20,14 @@ for PRUNE in {10,100,1000,10000,100000}; do
 	X="prob4_$PRUNE"
 	Y="entropy_prob4_$PRUNE"
 
+	salida="analisis_4gram_prunned_$PRUNE.csv"
+
 	echo "$X,$Y" |
 		cat - entropy_with_cache_* |
-		paste -d "," analisis_4gram.csv - |
-		python graficar_entropia.py -x $X -y $Y -o "plot_$PRUNE.png";
+		paste -d "," analisis_4gram.csv - > $salida
+
+	python graficar_entropia.py < $salida -x $X -y prob_cloze -o "plot1_$PRUNE.png";
+	python graficar_entropia.py < $salida -x $Y -y entropia_cloze -o "plot2_$PRUNE.png";
+	python graficar_entropia.py < $salida -x $X -y $Y -o "plot3_$PRUNE.png";
 
 done
